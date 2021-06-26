2e0fa6fc7e94c49b25914aa403a59fb3162e8e78
dev2033 <dev.elop.02@mail.ru>
2021-06-26 13:19:15 +0300

    qwe

--- tattoo/about/admin.py
+++ tattoo/about/admin.py
@@ -21,3 +21,7 @@ class InfoAdmin(admin.ModelAdmin):
     save_on_top = True
     list_display = ('id', 'name', 'views')
     list_display_links = ('id', 'name')
+
+
+admin.site.site_title = 'Административная панель сайта Тату-студии "Якорь"'
+admin.site.site_header = 'Административная панель сайта Тату-студии "Якорь"'
